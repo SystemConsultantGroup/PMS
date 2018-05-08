2e29ab7f9c1ae8583ff30161b03a554903ba2c30
EunChan Park <pec9399@naver.com>
2018-05-08 17:06:14 +0900

    [Fix] Added Path module for index.js file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var config = require('../config/config.json')[process.env.NODE_ENV || "development"];
-var viewPath = config.path;
+const express = require('express');
+const router = express.Router();
+const config = require('../config/config.json')[process.env.NODE_ENV || "development"];
+const viewPath = config.path;
+const path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next){
     res.sendFile(path.join(__dirname+'../..', viewPath.index, 'index.html'));
 });
 
 module.exports = router;
+	
