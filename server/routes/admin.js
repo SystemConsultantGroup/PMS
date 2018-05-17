3736235167db852cb02b1ffc33ce675adf082e65
KimYunji <gunvo88@naver.com>
2018-05-17 17:53:36 +0900

    [Add] Routing for Adding Project

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,4 +7,19 @@ router.get('/project', async (req, res, next) => {
 	res.send(projects);
 });
 
+//add project
+router.post('/project', async (req, res, next) => {
+	try{
+		const create = await models.project.create(req.body);
+		res.send({
+			result: true
+		});
+	}
+	catch(e){
+		res.send({
+			result: false
+		});
+	}
+});
+
 module.exports = router;
