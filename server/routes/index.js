161c836c5cac33a116b47f1629087d5fdb2da4e6
KimYunji <gunvo88@naver.com>
2018-05-24 11:28:12 +0900

    [Edit] Edit admin.js and index.js and user.js
    Add routing PUT '/admin/user/{uid}' to admin.js and POST '/user' to index.js and edit user.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,28 @@ const config = require('../config/config.json')[process.env.NODE_ENV || 'develop
 
 const viewPath = config.path;
 const path = require('path');
+const models = require('../models');
+const wrap = require('express-async-wrap');
+
 /* GET home page. */
 router.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}../..`, viewPath.index, 'index.html'));
 });
+
+// add user
+router.post('/user', wrap(async (req, res) => {
+  try {
+    const create = await models.user.create(req.body);
+    if (create) {
+      res.send({
+        result: true
+      });
+    }
+  } catch (e) {
+    res.send({
+      result: false
+    });
+  }
+}));
 
 module.exports = router;
