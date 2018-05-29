b6eeb2d1838570b4536f231f6cd5e408850b330b
lcw921 <leechangwon921@gmail.com>
2018-05-29 17:05:31 +0900

    [Add] Add routing to user.js
    Add user.js
    Edit app.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,38 +8,23 @@ const models = require('../models');
 const wrap = require('express-async-wrap');
 
 
-// 유저  정보 수정
-router.put('/user/:uid', wrap(async (req, res) => {
-  try {
-    const update = await models.user.update(req.body, {
-      where: {
-        uid: req.params.uid
-      }
-    });
-
-    if (update) {
-      res.send({
-        result: true
-      });
-    }
-  } catch (e) {
-    res.send({
-      result: false
-    });
-  }
-}));
 
 //유저 삭제 
-router.delete('/user', wrap(async (req, res) => {
+router.delete('/', wrap(async (req, res) => {
   try {
   	const dest = await models.user.destroy({
   		where : { uid : req.body.uid}
-  	})
+  	});
     if (dest) {
       res.send({
         result: true
       });
     }
+    else {
+      res.send({
+        result: false
+      });
+    }
   } catch (e) {
     res.send({
       result: false
@@ -47,5 +32,5 @@ router.delete('/user', wrap(async (req, res) => {
   }
 }));
 
-
+module.exports = router;
 
