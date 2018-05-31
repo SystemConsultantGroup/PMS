91f88df11883fbe2faab3b139b591f47f464a897
Jokuna <els9911@gmail.com>
2018-05-31 12:57:29 +0900

    [Edit] Edit project.js
    delete comment in project.js
    
    Merge remote-tracking branch 'refs/remotes/origin/master

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// const config = require('../config/config.json')[process.env.NODE_ENV || 'development'];
 
-// const viewPath = config.path;
-// const path = require('path');
 const models = require('../models');
 const wrap = require('express-async-wrap');
 
-//본인 소속 프로젝트 가져오기 
-//18번
-//1. get 방식으로 할때 user 의 uid가 필수적으로 필요?
-//본인 소속 인것을 제한시켜야 하지 않을까...
-
+//본인 소속 프로젝트 리스트 불러옴
 router.get('/:uid', wrap(async (req, res) => {
 	const assign_r = await models.assign_r.findAll({
 		where:{
