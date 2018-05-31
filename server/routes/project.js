53ea044574264a03166a94e292f3cd47fb4fe866
Jokuna <els9911@gmail.com>
2018-05-31 14:10:43 +0900

    [Add] Add project.js, edit app.js
    Add routing GET '/project/:uid' to project.js
    Add routing '/project' to app.js

--- server/routes/project.js
+++ server/routes/project.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const models = require('../models');
 const wrap = require('express-async-wrap');
 
-//본인 소속 프로젝트 리스트 불러옴
+//본인 소속 프로젝트 리스트 불러옴 get방식
 router.get('/:uid', wrap(async (req, res) => {
 	const assign_r = await models.assign_r.findAll({
 		where:{
