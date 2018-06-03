20523baf34081aa6fbff1a1f4cb3010c254e6cbd
김윤지 <gunvo88@naver.com>
2018-06-03 17:05:39 +0900

    [Edit] Edit routing to admin.js
    Remove try and catch statement and add admin author to routing GET '/project' and POST '/project' and PUT '/user/:uid'

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -10,53 +10,43 @@ const wrap = require('express-async-wrap');
 
 // get project list
 router.get('/project', wrap(async (req, res) => {
-  try {
+  if (req.session.user.auth === 1) {
     const projects = await models.project.findAll();
     if (projects) {
       res.send(projects);
     }
-  } catch (e) {
-    res.send({
-      result: false
-    });
   }
+  res.status(500).send('error');
 }));
 
 // add project
 router.post('/project', wrap(async (req, res) => {
-  try {
+  if (req.session.user.auth === 1) {
     const create = await models.project.create(req.body);
     if (create) {
       res.send({
         result: true
       });
     }
-  } catch (e) {
-    res.send({
-      result: false
-    });
   }
+  res.status(500).send('error');
 }));
 
 // 유저 이메일, 전화번호, 역할 정보 수정
 router.put('/user/:uid', wrap(async (req, res) => {
-  try {
+  if (req.session.user.auth === 1) {
     const update = await models.user.update(req.body, {
       where: {
         uid: req.params.uid
       }
     });
-
     if (update) {
       res.send({
         result: true
       });
     }
-  } catch (e) {
-    res.send({
-      result: false
-    });
   }
+  res.status(500).send('error');
 }));
 
 module.exports = router;
