c04bf2a098caeab1d80b30788fb64c4cef04d1c4
KoHyunsu <sce06147@naver.com>
2018-06-05 08:44:10 +0900

    [Edit] Edit login controller to apply SHA256

--- src/app/layout/login.controller.js
+++ src/app/layout/login.controller.js
@@ -4,13 +4,13 @@
     .controller('LoginController', LoginController);
 
   // 로그인 컨트롤러
-  function LoginController($log, $window, $sessionStorage, $document, $http, $state) {
+  function LoginController($log, $window, $sessionStorage, $document, $http, $state, SHA256) {
     const vm = this;
     vm.log = $log.log;
 
     vm.login = () => {
       // 백엔드에 인증 시도
-      $http.post('/rest/user/login', {
+      $http.post('/rest/login', {
         uid: vm.uid,
         pw: SHA256(vm.pw)
       }).then((result) => {
@@ -18,10 +18,26 @@
           $window.location.assign('/main');
         } else if (vm.uid == null) alert('아이디를 입력해주세요.');
         else alert('아이디 또는 비밀번호를 다시 확인해주세요.');
+        console.log('Login Error');
 
         $sessionStorage.putObject('session', result.data);
       });
     };
+
+    /* vm.login = () => {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.uid, vm.pw, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.uid, vm.pw);
+                    $location.path('/main');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                    console.log("Error")
+                }
+            });
+        }; */
+
 
     const input = $document[0].getElementById('password_input');
 
