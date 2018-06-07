4748eeed4d80e3e86374e7ecd5ba168e3e344a2d
EunChan Park <pec9399@naver.com>
2018-06-07 16:56:06 +0900

    [Fix] Assigned projet routing
    Fix '/rest/project/:uid'

--- server/routes/project.js
+++ server/routes/project.js
@@ -7,13 +7,11 @@ const wrap = require('express-async-wrap');
 
 // 본인 소속 프로젝트 리스트 불러옴 get방식
 router.get('/:uid', wrap(async (req, res) => {
-  const assign_r = await models.assign_r.findAll({
-    where: {
-      uid: req.params.uid
-    },
-    include: ['project']
+  const projects = await models.project.findAll({
+    where: { uid: req.params.uid }
   });
-  res.send(assign_r);
+
+  res.send(projects);
 }));
 
 
