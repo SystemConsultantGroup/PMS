6b94c3cb3e3a66154ce133f404d078dc34bccd22
MinJae Kang <39152134+mjk0618@users.noreply.github.com>
2018-06-27 21:40:04 +0900

    [Edit] Layout.controller.js and sidebar.html (#61)
    * [Edit] Main.html
    
    Disabled modify, delete button.
    
    * [Edit] Layout.controller.js / admin.sidebar.html and sidebar.html
    
    Disable some buttons on sidebar
    Add link on home button (to main page)
    
    * [Edit] Index.route.js and three other files
    
    Remove Korean and translate every word to English.
    
    * [Edit] Layout.controller.js and sidebar.html
    
    Show username on sidebar

--- src/app/layout/layout.controller.js
+++ src/app/layout/layout.controller.js
@@ -4,7 +4,7 @@
     .controller('LayoutController', LayoutController);
 
   /** @ngInject */
-  function LayoutController($log, $state, $stateParams, $mdSidenav) {
+  function LayoutController($log, $state, $stateParams, $mdSidenav, $http) {
     const vm = this;
     /* 교수 정보 */
     vm.state = $state;
@@ -23,13 +23,20 @@
       if (vm.act === 'none') vm.act = 'block';
       else if (vm.act === 'block') vm.act = 'none';
     };
+
+    $http.get('/rest/session').then((result) => {
+      //if (result.data.auth === 1) { vm.user = 'admin'; } else if (result.data.auth === 0 && result.data.auth > 1) { vm.user = 'user'; }
+      console.log(result.data.name);
+      vm.name = result.data.name;
+    });
+
     // link에 state이름, title에 사이드바에 띄우는 항목명
     vm.admin = [/*{
       link: 'notice',
       title: '공지사항',
       icon: 'announcement'
     }, /*{
-      link: 'subject_manage',
+      link: 'subject_manage', 
       title: '과목',
       icon: 'assignment'
     }, {
