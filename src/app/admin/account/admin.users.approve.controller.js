9c1619116c81dc4bde4818393c17e2cd1dd24058
MinJae Kang <minjae618@naver.com>
2018-07-02 14:41:57 +0900

    [Edit] Change Korean to English and fix refuse button error
    Edit admin.project.html and main.controller.js
    Fix refuse button at user approve page

--- src/app/admin/account/admin.users.approve.controller.js
+++ src/app/admin/account/admin.users.approve.controller.js
@@ -32,25 +32,30 @@
     vm.initUsers = () => {
       $http.get('/rest/admin/users/default').then((response) => {
         if (response.data.error) {
-          alert('승인 대상 유저가 존재하지 않습니다.');
+          alert('No users to approve.');
         }
         vm.user = response.data;
       });
     };
+    // 승인 대상 유저가 존재하지 않습니다.
     vm.approve = (uid) => {
       console.log(uid);
-      const cf = window.confirm('승인하시겠습니까?');
+      const cf = window.confirm('Approve?');
+      // 승인하시겠습니까?
       if (cf) {
         $http.put(`/rest/admin/users/approve/${uid}`);
-        alert('해당 유저가 승인되었습니다.');
+        alert('Approved.');
+        // 해당 유저가 승인되었습니다.
         $window.location.reload();
       }
     };
-    vm.refuse = (uid) => {
-      const cf = window.confirm('거절하시겠습니까?');
+    vm.delete = (uid) => {
+      const cf = window.confirm('Refuse?');
+      // 거절하시겠습니까?
       if (cf) {
         $http.delete(`/rest/admin/user/${uid}`);
-        alert('해당 유저가 삭제되었습니다.');
+        alert('Refused.');
+        // 해당 유저가 삭제되었습니다.
         $window.location.reload();
       }
     };
