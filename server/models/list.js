9d1b07a7d380005ff1925a43156a8b50a814d910
Jokuna <els9911@gmail.com>
2018-07-10 20:33:07 +0900

    [Edit] Fix Eslint Errors.

--- server/models/list.js
+++ server/models/list.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize) => {
   const list = sequelize.define('list', {},
-  {
-    tableName: 'list',
-    comment: '새 테이블 2'
-  });
+    {
+      tableName: 'list',
+      comment: '새 테이블 2'
+    });
   list.associate = (models) => {
     list.belongsTo(models.user, { foreignKey: { name: 'uid', allowNull: false }, onDelete: 'CASCADE', onUpdate: 'CASCADE' });
     list.belongsTo(models.todo, { foreignKey: { name: 'tdid', allowNull: false }, onDelete: 'CASCADE', onUpdate: 'CASCADE' });
