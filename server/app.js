bc5b9c6232d6c5b4c9ce1cfccf18e55cbe836ace
KoHyunsu <sce06147@naver.com>
2018-07-14 03:21:04 +0900

    [Edit] App.js to block access without admin's permission.

--- server/app.js
+++ server/app.js
@@ -85,7 +85,7 @@ const user = require('./routes/user');
 const project = require('./routes/project');
 
 app.use('/*', function (req, res, next) {
-  if (!(req.baseUrl.includes('login') || req.baseUrl.includes('user')) && (req.session.user === null || req.session.user.auth === 0)) {
+  if (!(req.baseUrl.includes('login') || req.baseUrl.includes('user')) && (req.session.user === undefined || req.session.user.auth === 0)) {
     res.redirect('/login');
   } else {
     next();
